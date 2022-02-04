9bce820330225120cf30683dfe506ac89bf789f8
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-02-04 09:37:24 +0700

    feat: added router

--- src/controllers/asset.controller.ts
+++ src/controllers/asset.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { isValidObjectId } from "mongoose";
 import { AssetModel } from "../models/asset.model";
 
 const getAllAssets = async (req: Request, res: Response) => {
@@ -9,5 +10,83 @@ const getAllAssets = async (req: Request, res: Response) => {
     });
 };
 
-export { getAllAssets };
+const getAssetById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if(!isValidObjectId(id)) { 
+        return res.status(400).json({
+            errors: [
+                {
+                    status: 400,
+                    title: 'Bad Request',
+                    message: 'Invalid paramater'
+                }
+            ]
+        }); 
+     }
+
+    try {
+        const asset = await AssetModel.findById({ '_id': id });
+
+        if (!asset) {
+            return res.status(404).json({
+                errors: [
+                    {
+                        status: 404,
+                        title: 'Not Found',
+                        message: `Id with ${id} not found`
+                    }
+                ]
+            });
+        }
+
+        return res.status(200).json({
+            data: asset
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
+    
+};
+
+const getPolygonAssets = async (req: Request, res: Response) => {
+    const assets = await AssetModel.find({ 'location.type' : 'Polygon' });
+
+    return res.status(200).json({
+        data: assets
+    });
+};
+
+const getPointAssets = async (req: Request, res: Response) => {
+    const assets = await AssetModel.find({ 'location.type': 'Point' });
+
+    return res.status(200).json({
+        data: assets
+    });
+};
+
+const createAsset = async (req: Request, res: Response) => {
+    const asset = req.body;
+    const created = AssetModel.create(asset);
+
+    if (!created) {
+        return res.status(400).json({
+            errors: [
+                {
+                    status: 400,
+                    title: 'Bad Request',
+                    message: 'Format tidak sesuai spesifikasi'
+                }
+            ]
+        })
+    }
+
+    return res.status(201).json({
+        message: 'Berhasil ditambahkan'
+    });
+};
+
+export { getAllAssets, getAssetById, getPointAssets, getPolygonAssets, createAsset };
 
--- src/routes/asset.route.ts
+++ src/routes/asset.route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getAllAssets } from "../controllers/asset.controller";
+import { getAllAssets, getAssetById, getPointAssets, getPolygonAssets, createAsset } from "../controllers/asset.controller";
 
 export const AssetRoute = () => {
     const router = Router();
 
     router.get('/assets', getAllAssets);
+    router.get('/asset/:id', getAssetById);
+    router.get('/assets/polygon', getPolygonAssets);
+    router.get('/assets/point', getPointAssets);
+    router.post('/assets', createAsset);
 
     return router;
 }
